dc9a41f5f2f4c61c723f58e18aedd2554745125c
Maxim Sinelnikov <maxim@sinelnikov.info>
2019-05-06 03:35:20 +0200

    readme

--- src/app/beer-service.service.ts
+++ src/app/beer-service.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { environment } from "../environments/environment";
 
 @Injectable({
   providedIn: "root"
 })
 export class BeerServiceService {
-  url = "http://localhost:3000";
+  url = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
